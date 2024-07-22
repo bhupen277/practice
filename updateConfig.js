1b7674c988064873bbec15ec7a6dfa5ce791c435
bhupen patil <bhupenpatil2326@gmail.com>
2024-07-23 00:47:04 +0530

    --- Auto Git Commit ---

--- updateConfig.js
+++ updateConfig.js
@@ -1,28 +1,41 @@
-// const fs = require('fs');
+const fs = require('fs');
+const { exec } = require('child_process');
 
-// // Initial JSON configuration object
-// let config = {
-//   "runOnStart": true,
-//   "updateInterval": 5,
-//   "logging": true,
-//   "silent": true,
-//   "commitMessage": "--- Auto Git Commit ---",
-//   "locale": "en-US",
-//   "timeZone": "Europe/Berlin"
-// };
+// Initial JSON configuration object
+let config = {
+  "runOnStart": true,
+  "updateInterval": 5,
+  "logging": true,
+  "silent": true,
+  "commitMessage": "--- Auto Git Commit ---",
+  "locale": "en-US",
+  "timeZone": "Europe/Berlin"
+};
 
-// // Generate the userName with the current time
-// let currentDate = new Date().toLocaleTimeString();
-// let userName = "commit by bhupen " + currentDate;
+// Generate the userName with the current time
+let currentDate = new Date().toLocaleTimeString();
+let userName = "commit by bhupen " + currentDate;
 
-// // Update the commitMessage in the config object
-// config.commitMessage = userName;
+// Update the commitMessage in the config object
+config.commitMessage = userName;
 
-// // Print the updated config to the console
-// console.log(config);
+// Save the updated config to a file
+fs.writeFile('config.json', JSON.stringify(config, null, 2), (err) => {
+  if (err) throw err;
+  console.log('The configuration file has been updated.');
 
-// // Optionally, save the updated config to a new file
-// fs.writeFile('updatedConfig.json', JSON.stringify(config, null, 2), (err) => {
-//   if (err) throw err;
-//   console.log('The file has been saved!');
-// });
+  // Run Git commands to commit the changes
+  exec('git add config.json', (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error adding file: ${stderr}`);
+      return;
+    }
+    exec(`git commit -m "${userName}"`, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`Error committing file: ${stderr}`);
+        return;
+      }
+      console.log('Changes have been committed.');
+    });
+  });
+});
